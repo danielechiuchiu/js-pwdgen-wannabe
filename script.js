9d237d2b22279a9eb25d0e1aa6d1749f2dadcd67
danielechiuchiu <daniele.chiuchiu@gmail.com>
2020-07-06 15:54:17 +0200

    final commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var nome = prompt('Scrivi il tuo nome:');
+
+var cognome = prompt('Scrivi il tuo cognome:');
+
+var colore = prompt('ora il tuo colore preferito:')
+
+var numero = prompt('infine inserisci un numero:');
+
+var password = nome + cognome + colore + numero;
+document.getElementById("password").innerHTML = "La tua Password è:"+ password;
